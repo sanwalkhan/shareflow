f85fd9d6d840c503da86be16882cb4351837828b
msaam112 <sam58malik@gmail.com>
2025-11-12 16:12:34 +0500

    dslfjkdg

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
-    plugins: ["nativewind/babel"], // ✅ plugin yahan
   };
 };
