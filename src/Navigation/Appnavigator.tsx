ddcbbd180e0985d9bc53b1a9b0f3494da64fcc02
Muqadas Tehseen <bsf2104415@ue.edu.pk>
2025-11-25 14:41:50 +0500

    Structure Issue
    Structure Issue

--- src/Navigation/Appnavigator.tsx
+++ src/Navigation/Appnavigator.tsx
@@ -1,22 +1,25 @@
-// src/Navigation/AppNavigator.tsx
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingScreen from '../Screens/LandingFolder/LandingScreen';
 import Admin from '../Screens/AdminFolder/Admin';
 import HomeLayout from '../Screens/HomeFolder/HomeLayout';
 import Dashboard from '../Screens/DashboardFolder/Dashboard';
-
 import HeaderFooterFile from '../Screens/HeaderFooterFolder/HeaderFooterFile';
 import HRFile from '../Screens/HRPanelFolder/HRFile';
 import Login from '../Screens/LoginFolder/Login';
 import Signup from '../Screens/SignupFolder/Signup';
 import VerifyEmail from '../Screens/VerifyEmailFolder/VerifyEmail';
 
-
 export type RootStackParamList = {
   LandingScreen: undefined;
-  AdminScreen: undefined;
+  Admin: undefined;
   HomeLayout: undefined;
+  Dashboard: undefined;
+  HeaderFooterFile: undefined;
+  HRFile: undefined;
+  Login: undefined;
+  Signup: undefined;
+  VerifyEmail: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -27,7 +30,7 @@ const AppNavigator: React.FC = () => {
       <Stack.Screen name="LandingScreen" component={LandingScreen} />
       <Stack.Screen name="Admin" component={Admin} />
       <Stack.Screen name="HomeLayout" component={HomeLayout} />
-      <Stack.Screen name="Dashbaord" component={Dashboard} />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="HeaderFooterFile" component={HeaderFooterFile} />
       <Stack.Screen name="HRFile" component={HRFile} />
       <Stack.Screen name="Login" component={Login} />
--- src/Screens/LandingFolder/LandingScreen.tsx
+++ src/Screens/LandingFolder/LandingScreen.tsx
@@ -1,15 +1,22 @@
-// src/screens/LandingScreen.tsx
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView } from 'react-native';
 
 // Components from LandingPages folder
 import HeroScreen from '../../components/LandingPages/HeroScreen';
+import BusinessScreen from '../../components/LandingPages/BusinessScreen';
+import Cards from '../../components/LandingPages/Cards';
+import InfoScreen from '../../components/LandingPages/InfoScreen';
+import Marketing from '../../components/LandingPages/Marketing';
 
 const LandingScreen: React.FC = () => {
   return (
-    
+    <ScrollView>
       <HeroScreen />
-   
+      <BusinessScreen />
+      <Cards />
+      <InfoScreen />
+      <Marketing />
+    </ScrollView>
   );
 };
 
